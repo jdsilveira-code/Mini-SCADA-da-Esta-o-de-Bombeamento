59a4532caae94724501500af76f658a7fac72640
LeadroLingueta <leanndro.gamer.br@gmail.com>
2026-06-16 09:53:52 -0300

    adicionado o timestemp ao codigo e começando a ter o que precisa para o json ficar completo

--- dispositivo_cpp/src/Sensores.cpp
+++ dispositivo_cpp/src/Sensores.cpp
@@ -1,7 +1,47 @@
 #include "Sensores.hpp"
+#include <chrono>
+#include <ctime>
+#include <iomanip>
+#include <sstream>
 
 double Sensor::getValorAtual() {
     return ValorAtual;
+}
+
+std::string Sensor::getTag() {
+    return Tag;
+}
+
+std::string Sensor::getUnidadeMedida() {
+    return UnidadeMedida;
+}
+
+void Sensor::atualizarTimestamp() {
+    auto agora = std::chrono::system_clock::now();
+    std::time_t tempo = std::chrono::system_clock::to_time_t(agora);
+    std::tm tempoUtc{};
+
+#ifdef _WIN32
+    gmtime_s(&tempoUtc, &tempo);
+#else
+    gmtime_r(&tempo, &tempoUtc);
+#endif
+
+    std::ostringstream saida;
+    saida << std::put_time(&tempoUtc, "%Y-%m-%dT%H:%M:%SZ");
+    Timestamp = saida.str();
+}
+
+std::string Sensor::getTimestamp() {
+    return Timestamp;
+}
+
+std::string Sensor::getStatus() {
+    return Status;
+}
+
+void Sensor::setStatus(std::string novoStatus) {
+    Status = novoStatus;
 }
 
 SensorNivel::SensorNivel(std::string tag, float max, float min) {
@@ -13,6 +53,7 @@ SensorNivel::SensorNivel(std::string tag, float max, float min) {
 
 void SensorNivel::ler() {
     ValorAtual = gerador.decimal(ValorMin, ValorMax);
+    atualizarTimestamp();
 }
 
 SensorRadiacao::SensorRadiacao(std::string tag, float max, float min) {
@@ -24,6 +65,7 @@ SensorRadiacao::SensorRadiacao(std::string tag, float max, float min) {
 
 void SensorRadiacao::ler() {
     ValorAtual = gerador.decimal(ValorMin, ValorMax);
+    atualizarTimestamp();
 }
 
 SensorTemp::SensorTemp(std::string tag, float max, float min) {
@@ -35,6 +77,7 @@ SensorTemp::SensorTemp(std::string tag, float max, float min) {
 
 void SensorTemp::ler() {
     ValorAtual = gerador.decimal(ValorMin, ValorMax);
+    atualizarTimestamp();
 }
 
 SensorVazao::SensorVazao(std::string tag, float max, float min) {
@@ -46,4 +89,5 @@ SensorVazao::SensorVazao(std::string tag, float max, float min) {
 
 void SensorVazao::ler() {
     ValorAtual = gerador.decimal(ValorMin, ValorMax);
+    atualizarTimestamp();
 }
