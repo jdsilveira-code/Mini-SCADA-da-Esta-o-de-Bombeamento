cef94f1f8916450109aa4de6226a3e09c697fec7
jdsilveira-code <jd.silverg@gmail.com>
2026-06-22 12:51:54 -0300

    Implementacao dos alarmes no main e escrita dos mesmos no arquivo json. Criacao da estrutura para o gerarJsonAlarme

--- dispositivo_cpp/src/JsonExporter.cpp
+++ dispositivo_cpp/src/JsonExporter.cpp
@@ -1,4 +1,5 @@
 #include "JsonExporter.hpp"
+#include "Alarmes.hpp"
 #include <sstream>
 
 std::string JsonExporter::gerarJsonLeitura(Sensor& sensor) {
@@ -9,6 +10,17 @@ std::string JsonExporter::gerarJsonLeitura(Sensor& sensor) {
 		 << "\"unidade\": \"" << sensor.getUnidadeMedida() << "\", "
 		 << "\"timestamp\": \"" << sensor.getTimestamp() << "\", "
 		 << "\"status\": \"" << sensor.getStatus() << "\""
+		 << "}";
+	return json.str();
+}
+
+std::string JsonExporter::gerarJsonAlarme(EstrategiaAlarme& alarme) {
+	std::ostringstream json;
+	json << "{"
+		 << "\"tag\": \"" << alarme.getTag() << "\", "
+		 << "\"tipo\": \"alarme\", "
+		 << "\"status\": \"" << alarme.getStatusAlarme() << "\", "
+		 << "\"timestamp\": \"" << alarme.getTimestamp() << "\""
 		 << "}";
 	return json.str();
 }
